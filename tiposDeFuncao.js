6bc6f0c09efd6d702ba69e5a0a4d87a3ce57b258
MirBrasil <87242088+MirBrasil@users.noreply.github.com>
2022-03-26 18:00:57 -0300

    Create tiposDeFuncao.js

---
+++ tiposDeFuncao.js
@@ -0,0 +1,38 @@
+/*  tipos de função
+
+função declarativa
+declarada usando a palavra reservada function seguida do nome da funçao, parênteses "()" e chaves "{}"
+nomeada obrigatoriamente
+
+function nomeDaFuncao() {
+    alert('Sou uma função declarativa!');
+}
+devemos chamar a função para ver o resultado
+chamando a função
+nomeDaFuncao();
+
+
+expressão de função
+funções atribuídas à expressões
+nomeação opcional
+
+com nomeação
+var funcao = function funcao() {
+    alert('Sou uma expressão de função nomeada!');
+} 
+
+sem nomeação
+var funcao = function() {
+    alert('Sou uma expressão de função não nomeada!')
+}
+
+
+arrow Function
+função de expressão de sintaxe curta
+sempre anônima, não nomeada
+deve ser declarada com parênteses "()", seguido de "=>" e depois chaves "{}"
+
+var funcao = () => {
+    alert('Sou uma função anônima!')
+};
+funcao(); */
